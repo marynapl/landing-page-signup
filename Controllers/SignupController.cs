a60c99b40628eb90280fa5a2d6e17a6b7e791a92
Maryna <kushnirmarinka@gmail.com>
2020-07-03 10:28:26 -0400

    Create data model and add Subscriber entity. Add EntityFramework package. Setup EF to use LocalDB (Subscription.mdf).

---
+++ Controllers/SignupController.cs
@@ -0,0 +1,28 @@
+﻿using LandingPageSignup.DAL;
+using LandingPageSignup.Models;
+using System;
+using System.Web.Http;
+
+namespace LandingPageSignup.Controllers
+{
+    public class SignupController : ApiController
+    {
+        public void Post([FromBody]string email, [FromBody] string name)
+        {
+            // TODO: implement api
+
+            //using (SubscriptionContext dbContext = new SubscriptionContext())
+            //{
+            //    Subscriber subscriber = new Subscriber()
+            //    {
+            //        Name = "Irene",
+            //        Email = "myemail2@google.com",
+            //        CreateDate = DateTime.Now
+            //    };
+
+            //    dbContext.Subscribers.Add(subscriber);
+            //    dbContext.SaveChanges();
+            //}
+        }
+    }
+}
---
+++ DAL/SubscriptionContext.cs
@@ -0,0 +1,20 @@
+﻿using LandingPageSignup.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace LandingPageSignup.DAL
+{
+    public class SubscriptionContext : DbContext
+    {
+        public SubscriptionContext() : base("SubscriptionContext")
+        {
+        }
+
+        public DbSet<Subscriber> Subscribers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
